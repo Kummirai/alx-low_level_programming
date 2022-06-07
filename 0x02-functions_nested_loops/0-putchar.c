546a1f7796c53c87efee2a137d69de9dad442902
Kummirai <ajaxmilton@hotmail.com>
2022-06-07 11:14:13 -0700

    Print _putchar then newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print _putchaar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _putchar(char c)
+
+		char c[] = "_putchar";
+		int MIL;
+
+		for (MIL = 0, c[MIL] != '\0', MIL++)
+{
+	_putchar(c[MIL]);
+}
+	_putchar('\n');
+	return (0);
+}

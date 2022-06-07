0b3f386e16733a181045751e2048e077cc92332c
Kummirai <ajaxmilton@hotmail.com>
2022-06-07 12:04:40 -0700

    Fix output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,18 @@
 #include "main.h"
 
 /**
- * main - Print _putchaar
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int _putchar(char c)
+	char word[] = "_putchar";
+	int MIL;
 
-		char c[] = "_putchar";
-		int MIL;
+	for (MIL = 0; MIL < 8; MIL++)
 
-		for (MIL = 0, c[MIL] != '\0', MIL++)
-{
-	_putchar(c[MIL]);
-}
+	_putchar(word[MIL]);
 	_putchar('\n');
 	return (0);
 }

923b41c1b4ca5224567a94d43505a2a6046e6af2
Kummirai <ajaxmilton@hotmail.com>
2022-06-28 14:05:39 -0700

    Fix code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,35 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- *
- * Return: pointer of an array of chars
+ * str_concat - Entry point
+ *@s1: string 1
+ *@s2: string 2
+ * Return: pointer should point to a newly allocated space in memory or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *a;
-	unsigned int i, j, k, max;
+	char *str;
+	unsigned int i;
+	int j;
+	int k;
+	int l;
 
+	l = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
+	for (j = 0; s1[j] != '\0'; j++)
 		;
-	for (j = 0; s2[j] != '\0'; j++)
-
-	a = malloc(sizeof(char) * (i + j + 1));
-	if (a == NULL)
+	for (k = 0; s2[k] != '\0'; k++)
+		;
+	str = (char *)malloc((j + k + 1) * sizeof(char));
+	if (str == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
-	for (k = 0; k < i; k++)
-		a[k] = s1[k];
-	max = j;
-	for (j = 0; j <= max ; j++, k++)
-		a[k] = s2[j];
-	return (a);
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (; s2[l] != '\0'; i++)
+	{
+		str[i] = s2[l];
+		l++;
+	}
+	return (str);
 }

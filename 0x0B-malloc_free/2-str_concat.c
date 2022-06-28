dbbed65bda6de056e390c10e46cbd21680e771f6
Kummirai <ajaxmilton@hotmail.com>
2022-06-28 12:08:36 -0700

    Fox code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *a;
-	unsigned int i, j, k, n;
+	unsigned int i, j, k, max;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -24,12 +24,12 @@ char *str_concat(char *s1, char *s2)
 	a = malloc(sizeof(char) * (i + j + 1));
 	if (a == NULL)
 	{
-		free(a);
-		return (NULL);
+	return (NULL);
 	}
 	for (k = 0; s1[k] != '\0'; k++)
 		a[k] = s1[k];
-	for (n = 0; s2[n] != '\0'; n++, k++)
-		a[k] = s2[n];
+	max = j;
+	for (j = 0; j <= max ; j++, k++)
+		a[k] = s2[j];
 	return (a);
 }

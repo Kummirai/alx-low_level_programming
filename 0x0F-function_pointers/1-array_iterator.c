378c77a5dcfdf150cb980dcfc7b44ad5925534ef
Kummirai <ajaxmilton@hotmail.com>
2022-07-05 23:58:29 -0700

    Execute a function as a parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executed another function
+ * @size: size of the array
+ * @action: function to use on array
+ * @array: array to iterate over
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i <= size; i++)
+		action(array[i]);
+}

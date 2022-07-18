19eba4679ab2592bd8eb84d1a2c57988f1c252f1
Kummirai <ajaxmilton@hotmail.com>
2022-07-18 22:46:05 +0200

    Remove type casting

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *ptr;
 
-	ptr =(listint_t*) malloc(sizeof(listint_t));
+	ptr = malloc(sizeof(listint_t));
 	ptr->n = n;
 	ptr->next = NULL;
 

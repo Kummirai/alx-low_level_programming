ad6a6585e5ff3c23060b1f0fd66483d96c4938a0
Kummirai <ajaxmilton@hotmail.com>
2023-12-01 14:06:22 -0800

    Apped text to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,21 +12,18 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *file;
 
-	if (filename == NULL) {
-        return -1;
-    }
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content == NULL) {
-        return 1;
-    }
+	if (text_content == NULL)
+		return (-1);
+	file = fopen(filename, "a");
 
-    file = fopen(filename, "a");
-    if (file == NULL) {
-        return -1;
-    }
+	if (file == NULL)
+		return (-1);
 
-    fputs(text_content, file);
+	fputs(text_content, file);
 
-    fclose(file);
-    return 1;
+	fclose(file);
+	return (1);
 }

6f08aac89870a3f934921c8b51700fe2b436f112
Kummirai <ajaxmilton@hotmail.com>
2024-05-08 22:58:59 +0200

    Update Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,10 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print(int *array, size_t lower, size_t upper);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* _SEARCH_ALGOS_H_ */
